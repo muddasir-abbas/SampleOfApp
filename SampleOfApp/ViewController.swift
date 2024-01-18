46cbe761a48a03285c3ad728e6e3962000f958cb
Muddasir Abbas <115025090+muddasir-abbas@users.noreply.github.com>
2024-01-18 15:45:17 +0500

    Initial Commit

---
+++ SampleOfApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleOfApp
+//
+//  Created by Macbook on 18/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
